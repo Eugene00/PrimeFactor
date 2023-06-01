fb4160d90b42dec26584883323a05e63d67e3bc8
Seokhwan <sukkakim@gmail.com>
2023-06-01 15:44:51 +0900

    step 2: add a case of number 2 & use test fixture

--- Prime-Test/test.cpp
+++ Prime-Test/test.cpp
@@ -1,9 +1,21 @@
 #include "pch.h"
 #include "../PrimeFactor/prime_factor.cpp"
+#include <vector>
+
+class PrimeFixture : public testing::Test
+{
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected{};
+};
 
 using namespace  std;
-TEST(PrimeFactors, Of1) {
-	PrimeFactor prime_factor;
-	vector<int> expected = {};
+TEST_F(PrimeFixture, Of1) {
+	const vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFixture, Of2) {
+	const vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
