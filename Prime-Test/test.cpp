960b397ba99c4052deb751d6a51aa6056c49be5b
Seokhwan <sukkakim@gmail.com>
2023-06-01 16:04:24 +0900

    step 7: consider the case where input number = 6

--- Prime-Test/test.cpp
+++ Prime-Test/test.cpp
@@ -29,3 +29,8 @@ TEST_F(PrimeFixture, Of4) {
 	const vector<int> expected = { 2, 2 };
 	EXPECT_EQ(expected, prime_factor.of(4));
 }
+
+TEST_F(PrimeFixture, Of6) {
+	const vector<int> expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
